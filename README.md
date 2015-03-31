0b0353c0f8f77362553f2299637b48feb657e4f7
liupeng <79144876@qq.com>
2015-03-31 09:34:08 +0800

    add demo request

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # android-async-http-loopj-with-cache
+
+#Easy to use
+###1.create client
+````
+       asyncHttpClient = new CacheAsyncHttpClient();
+        asyncHttpClient.setTimeout(30000);
+        asyncHttpClient.setConnectTimeout(50000);
+        asyncHttpClient.setEnableRedirects(true);
+````
+###2.getResponseCache
+```
+  private RequestHandle getResponseCache(Context context, String url, boolean findCacheBreak, CacheBaseGsonHttpResponseHandler responseHandler) {
+        return asyncHttpClient.getCache(context, url, null, null, findCacheBreak, responseHandler);
+    }
+```
+###3.do some request
+
+```
+  getResponseCache(this, targetUrl, false, new CacheBaseGsonHttpResponseHandler<Entity>(Entity.class) {
+            @Override
+            public void onCacheSuccess(Entity response) {
+
+            }
+
+            @Override
+            public void onGsonSuccess(Entity result) {
+
+            }
+
+            @Override
+            public void onGsonFail(int statusCode) {
+
+            }
+        });
+
+```
--- src/com/eusoft/MyActivity.java
+++ src/com/eusoft/MyActivity.java
@@ -1,11 +1,19 @@
 package com.eusoft;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import com.eusoft.loopj.CacheAsyncHttpClient;
+import com.eusoft.loopj.core.CacheBaseGsonHttpResponseHandler;
 import com.eusoft.utils.JniApi;
 import com.eusoft.R;
+import com.loopj.android.http.RequestHandle;
 
 public class MyActivity extends Activity {
+    private class Entity{
+        private String somePro;
+    }
+    private CacheAsyncHttpClient asyncHttpClient;
     /**
      * Called when the activity is first created.
      */
@@ -14,5 +22,52 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         JniApi.appcontext = this;
         setContentView(R.layout.activity_main);
+        initClient();
+        demoRequest("you url here");
+    }
+
+    /**
+     * init client
+     */
+    private void initClient() {
+        asyncHttpClient = new CacheAsyncHttpClient();
+        asyncHttpClient.setTimeout(30000);
+        asyncHttpClient.setConnectTimeout(50000);
+        asyncHttpClient.setEnableRedirects(true);
+    }
+
+
+    /**
+     * do request some url data
+     * @param targetUrl url
+     */
+    private void demoRequest(String targetUrl) {
+        getResponseCache(this, targetUrl, false, new CacheBaseGsonHttpResponseHandler<Entity>(Entity.class) {
+            @Override
+            public void onCacheSuccess(Entity response) {
+
+            }
+
+            @Override
+            public void onGsonSuccess(Entity result) {
+
+            }
+
+            @Override
+            public void onGsonFail(int statusCode) {
+
+            }
+        });
+
+    }
+
+    /**
+     * get response
+     *
+     * @param url             target url
+     * @param responseHandler callback
+     */
+    private RequestHandle getResponseCache(Context context, String url, boolean findCacheBreak, CacheBaseGsonHttpResponseHandler responseHandler) {
+        return asyncHttpClient.getCache(context, url, null, null, findCacheBreak, responseHandler);
     }
 }
